fb511f4e79eb9280a32725a67bd27c85be248db6
Jorge Valencia Abad <jorgevalenciaabad18@gmail.com>
2023-04-14 08:39:59 +0200

    Modified Employee.java class

--- MyPetsWeb/src/main/java/it/jorge/MyPetsWeb/model/Employee.java
+++ MyPetsWeb/src/main/java/it/jorge/MyPetsWeb/model/Employee.java
@@ -1,5 +1,27 @@
 package it.jorge.MyPetsWeb.model;
 
-public class Employee {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="empleados")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "tel")
+    private String tel;
+    @Column(name = "pass")
+    private String pass;
 }
